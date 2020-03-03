5489ff4390c4dc382a8b8239df936fb9e21517b2
Jack Stanley <jackstanley@amdg.rockhursths.edu>
2020-03-03 08:48:57 -0600

    adventure

---
+++ Adventure.py
@@ -0,0 +1,17 @@
+
+def intro():
+    print("Welcome")
+    name = input("Who are you? ")
+    welcome(name)
+    
+def welcome(name):
+    print("Welcome " + name)
+
+def gender():
+    gender = input("Choose Your Gender: ")
+
+def race():
+    race = input("Choose Your Race: 
+
+intro()
+gender()
