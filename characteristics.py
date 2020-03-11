c4cd85b461679922d95c22be174c67ee511f4290
Jack Stanley <jackstanley@amdg.rockhursths.edu>
2020-03-11 08:42:18 -0500

    characteristics

--- characteristics.py
+++ characteristics.py
@@ -1,2 +1,57 @@
+import random
+
 class child(object):
     race = ["white", "black", "aisan", "hispanic", "jamaican"]
+    gender = ["male", "female"]
+    race = ["white", "black", "aisan", "hispanic", "jamaican"]
+    size = ["micro","3","4","5","6","7","8","9","10","12","They PACKIN"]
+    driving = ["good","bad"]
+
+    def __init__(self,race)
+    if(race == "white"):
+        choice = random.randint(1,4)
+        if(choice == 1):
+            size = "4"
+        elif(choice == 2):
+            size = "5"
+        elif(choice == 3):
+            size = "6"
+        elif(choice == 4):
+            size = "7"
+    if(race == "black"):
+        choice2 = random.randint(1,5)
+        if(choice2 == 1):
+            size = "7"
+        if(choice2 == 2):
+            size = "8"
+        if(choice2 == 3):
+            size = "9"
+        if(choice2 == 4):
+            size = "10"
+        if(choice2 == 5):
+            size = "12"
+    if(race == "aisan")
+        choice3 = random.randint(1,2)
+        if(choice3 == 1):
+            size = "micro"
+        if(choice3 == 2):
+            size = "3"
+    if(race == "hispanic")
+        choice4 = random.randint(1,4)
+        if(choice4 == 1):
+            size = "5"
+        if(choice4 = 2):
+            size = "6"
+        if(choice4 = 3):
+            size = "7"
+        if(choice4 = 4):
+            size = "8"
+    if(race == "jamaican")
+        choice4 = random.randint(1,2)
+        if(choice5 == 1):
+            size = "12"
+        if(choice5 == 2):
+            size = "They PACKIN"
+            
+        
+
